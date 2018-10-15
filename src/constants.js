fd9e41abec0dcd40bf602d0fae711488942b9c07
Fofir <ofir.weinstock@gmail.com>
2018-10-15 14:34:31 +0200

    Added a constants SCOOTER_MODEL_FILTERS
    * Will be used for the filter by scooter models feature

--- src/constants.js
+++ src/constants.js
@@ -2,3 +2,9 @@ export const SCOOTER_MODELS = {
   GOGORO_V1: 'Gogoro 1st edition',
   GOGORO_V2: 'Gogoro 2nd edition',
 };
+
+export const SCOOTER_MODEL_FILTERS = {
+  ALL: 'all',
+  GOGORO_V1: SCOOTER_MODELS.GOGORO_V1,
+  GOGORO_V2: SCOOTER_MODELS.GOGORO_V2,
+};

6582e44ced65f489d027e0a290c00dc7201cfd65
Fofir <ofir.weinstock@gmail.com>
2018-10-16 16:31:54 +0300

    Changed the method to get all scooters to be static
    * There was no real need for this class to instantiate if we
    are not passing any options for now to be saved as an actual
    axios api in the instance.

--- src/ScootersApi.js
+++ src/ScootersApi.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const BASE_URL = 'https://qc05n0gp78.execute-api.eu-central-1.amazonaws.com/prod';
 
 class ScootersApi {
-  all() {
+  static all() {
     return axios.get(`${BASE_URL}/scooters`)
       .then(resp => resp.data);
   }
--- src/ScootersApi.test.js
+++ src/ScootersApi.test.js
@@ -6,14 +6,8 @@ jest.mock('axios');
 axios.get.mockReturnValue(Promise.resolve({ data: apiMockResponse }));
 
 describe('ScootersApi', () => {
-  it('instantiates without crashing', () => {
-    const api = new ScootersApi();
-    expect(api).toBeDefined();
-  });
-
   it('gets a list of scooters from the API endpoint /scooters', async () => {
-    const api = new ScootersApi();
-    const result = await api.all();
+    const result = await ScootersApi.all();
     expect(result).toHaveProperty('meta');
     expect(result).toHaveProperty('data');
   });

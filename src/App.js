dfb72483e78d9dec4cf0a77a1983d8c8ca20f530
Fofir <ofir.weinstock@gmail.com>
2018-10-16 16:16:47 +0300

    Added the initial app state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
+import { SCOOTER_MODEL_FILTERS, DISPLAY_TYPES } from './constants';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      scooters: {},
+      scooterIds: [],
+      isInitialAppStateLoaded: false,
+      areScootersLoaded: false,
+      scooterModelFilter: SCOOTER_MODEL_FILTERS.ALL,
+      displayType: DISPLAY_TYPES.TABLE,
+    };
+  }
+
   render() {
     return (
       <div className="App">

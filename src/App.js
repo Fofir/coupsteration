c12b4a1aa82920db9df0327869027926fbd047cf
Fofir <ofir.weinstock@gmail.com>
2018-10-16 17:54:31 +0300

    Added the <ScooterTable /> and <Select /> for scooter type filters
    * Hooked those 2 components to the <App />
    * Added asome basic layout with bootstrap classes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import keyBy from 'lodash/keyBy';
-import { SCOOTER_MODEL_FILTERS, DISPLAY_TYPES } from './constants';
+import { SCOOTER_MODELS, SCOOTER_MODEL_FILTERS, DISPLAY_TYPES } from './constants';
 import ScootersApi from './ScootersApi';
+import ScootersTable from './ScootersTable';
+import Select from './Select';
+
+const scooterModelOptions = [
+  { label: 'All', value: SCOOTER_MODEL_FILTERS.ALL },
+  { label: SCOOTER_MODELS.GOGORO_V1, value: SCOOTER_MODEL_FILTERS.GOGORO_V1 },
+  { label: SCOOTER_MODELS.GOGORO_V2, value: SCOOTER_MODEL_FILTERS.GOGORO_V2 },
+];
 
 class App extends Component {
   constructor(props) {
@@ -57,8 +65,34 @@ class App extends Component {
   }
 
   render() {
+    const {
+      scooterIds,
+      scooters,
+      isInitialAppStateLoaded,
+    } = this.state;
+
     return (
       <div className="App">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <h1>Coupsteration</h1>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-12">
+              <Select
+                disabled={!isInitialAppStateLoaded}
+                options={scooterModelOptions}
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-12">
+              <ScootersTable scooters={scooterIds.map(id => scooters[id])} />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }

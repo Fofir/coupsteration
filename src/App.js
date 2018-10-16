93e36bdb6be3c863096ca325d4f398a442c267e0
Fofir <ofir.weinstock@gmail.com>
2018-10-16 16:36:27 +0300

    Added a method for <App /> to fetch the scooter list
    * This method is called in componentWillMount and when it finishes
    the application has finished initializing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import keyBy from 'lodash/keyBy';
 import { SCOOTER_MODEL_FILTERS, DISPLAY_TYPES } from './constants';
+import ScootersApi from './ScootersApi';
 
 class App extends Component {
   constructor(props) {
@@ -12,7 +14,36 @@ class App extends Component {
       areScootersLoaded: false,
       scooterModelFilter: SCOOTER_MODEL_FILTERS.ALL,
       displayType: DISPLAY_TYPES.TABLE,
+      hasScooterLoadingError: false,
     };
+  }
+
+  componentWillMount() {
+    this.fetchScooters();
+  }
+
+  fetchScooters = () => {
+    this.setState({
+      areScootersLoaded: false,
+      hasScooterLoadingError: false,
+    });
+
+    return ScootersApi.all()
+      .then(({ meta, data }) => {
+        this.setState({
+          scooters: keyBy(data.scooters, 'id'),
+          scooterIds: data.scooters.map(scooter => scooter.id),
+          areScootersLoaded: true,
+          isInitialAppStateLoaded: true,
+        });
+      })
+      .catch((error) => {
+        this.setState({
+          isInitialAppStateLoaded: true,
+          areScootersLoaded: true,
+          scooterLoadingError: true,
+        });
+      })
   }
 
   changeDisplayType = (displayType) => {
